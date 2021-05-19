f8988c85bf3538b64448d2504de122ed9ec473ed
nirajdahal09@gmail.com <nirajdahal09@gmail.com>
2021-05-20 01:02:33 +0545

    completion

--- AirlineSendAgent/Dtos/FlightDetailChangePayloadDto.cs
+++ AirlineSendAgent/Dtos/FlightDetailChangePayloadDto.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace TravelAgentWeb.Dtos
+namespace AirlineSendAgent.Dtos
 {
-    public class FlightDetailUpdateDto
+    public class FlightDetailChangePayloadDto
     {
         public string WebhookURI { get; set; }
         public string Publisher { get; set; }
--- AirlineSendAgent/Program.cs
+++ AirlineSendAgent/Program.cs
@@ -1,4 +1,12 @@
 ﻿using System;
+using System.IO;
+using AirlineSendAgent.App;
+using AirlineSendAgent.Client;
+using AirlineSendAgent.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace AirlineSendAgent
 {
@@ -6,7 +14,29 @@ namespace AirlineSendAgent
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddUserSecrets(typeof(Program).Assembly, optional:true)
+                .AddEnvironmentVariables()
+                .AddCommandLine(args);
+
+            var config = builder.Build();
+            
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>{
+                  services.AddSingleton<IAppHost, AppHost>();
+                  services.AddSingleton<IWebhookClient, WebhookClient>();
+                  services.AddDbContext<SendAgentDbContext>(opt => opt.UseSqlServer ("server=(localdb)\\MSSQLLocalDB; database=AirlineWeb; Integrated Security=true"));
+                  //(config.GetConnectionString("AirlineConnection")));
+                                                                
+                  
+                    
+                  services.AddHttpClient();
+                }).Build();
+
+            host.Services.GetService<IAppHost>().Run();
         }
     }
 }
